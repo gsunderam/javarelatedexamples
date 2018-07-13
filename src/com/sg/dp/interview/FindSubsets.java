bf44260fd91d5660ed807572a1180dc2c4102084
Sunderam G <gsunder23@rediffmail.com>
2018-07-13 10:28:09 -0700

    Find subsets

---
+++ src/com/sg/dp/interview/FindSubsets.java
@@ -0,0 +1,68 @@
+package com.sg.dp.interview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static com.sg.dp.log.Logger.stdout;
+
+/**
+ * Created by chandrashekar on 7/12/2018.
+ */
+public class FindSubsets {
+    public  List<List<Integer>> findSubsets(List<Integer> inputList) {
+        if (inputList == null || inputList.isEmpty()) return Collections.emptyList();
+
+        return getSubsets(inputList);
+    }
+
+    private List<List<Integer>> getSubsets(List<Integer> inputList) {
+        if (inputList == null || inputList.isEmpty()) {
+            List<List<Integer>> list = new ArrayList<>();
+            list.add(Collections.EMPTY_LIST);
+            return list;
+        }
+
+        Integer first = inputList.get(0);
+        List<Integer> subList = inputList.subList(1, inputList.size());
+        List<List<Integer>> subsets = getSubsets(subList);
+        return combine(first, subsets);
+    }
+
+    /**
+     * 1) Create a result list
+     * 2) Loop through the subsets, get each set and add to the result list
+     * 3) Now add first to the list of each subsets and append them to the result list
+     * @param first
+     * @param subsets
+     */
+    private List<List<Integer>> combine(Integer first, List<List<Integer>> subsets) {
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        subsets.stream().forEach(resultList::add);
+
+        return concatenate(first, subsets, resultList);
+    }
+
+    private List<List<Integer>> concatenate(Integer first, List<List<Integer>> subsets, List<List<Integer>> resultList) {
+        subsets.stream().forEach(subset -> {
+            List<Integer> list = new ArrayList<>();
+            list.add(first);
+            list.addAll(subset);
+            resultList.add(list);
+        });
+
+        return resultList;
+    }
+
+    private boolean isSum16(List<Integer> list, Predicate predicate) {
+        return predicate.test(list);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = Arrays.asList(2, 4, 6, 10);
+        stdout(new FindSubsets().findSubsets(inputList));
+    }
+}
