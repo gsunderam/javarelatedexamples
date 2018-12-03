a849e70be5f3d500bd387b9264afb83a678cab9b
Sunderam G <gsunder23@rediffmail.com>
2018-12-02 21:41:27 -0700

    median of 2 arrays

--- src/com/sg/dp/interview/MedianOfArrays.java
+++ src/com/sg/dp/interview/MedianOfArrays.java
@@ -6,20 +6,29 @@ import static com.sg.dp.log.Logger.stdout;
  * Created by chandrashekar on 11/25/2018.
  */
 public class MedianOfArrays {
-    private static final int [] a = {2, 7, 11, 14, 25, 34};
-    private static final int [] b = {1, 4, 5, 6, 12, 15, 35};//, 44, 55, 66, 200};
+    private static final int [] a = {1, 3, 5, 7, 12, 15, 17, 21, 23, 33, 45, 47, 48, 51, 54, 55, 100};
+    private static final int [] b = {2, 4, 6, 8, 11, 14, 16, 24, 25, 29, 32, 46, 49, 53, 56, 59, 65};//, 44, 55, 66, 200};
 
     private static final int NUMBER_OF_ELEMENTS = a.length + b.length;
     private static final int MED_INDEX = NUMBER_OF_ELEMENTS / 2 + 1;
 
     public static void main(String[] args) {
         stdout("Median index: " + MED_INDEX);
+        if (a.length == 0 && b.length == 0) {
+            stdout("Arrays are empty. Please provide valid values");
+            System.exit(0);
+        }
+
         findMedianAuxArray();
         findMedianInPlace();
     }
 
+    /**
+     * This algorithm is faster for medium sized arrays by 3 times
+     */
     private static void findMedianAuxArray() {
         int i = 0, j = 0, k = 0;
+        long start = System.nanoTime();
         int [] c = new int[MED_INDEX];
 
         while(k <= MED_INDEX - 1) {
@@ -33,29 +42,48 @@ public class MedianOfArrays {
             }
         }
 
-        stdout("i: " + i + " j: " + j);
-        stdout("Median is : " + determineMedian(c));
+//        stdout("i: " + i + " j: " + j);
+        double median = determineMedian(c);
+        long time_aux_array = System.nanoTime() - start;
+        stdout("Time taken for Auxillary array: " + time_aux_array / 1000);
+        stdout("Median is : " + median);
     }
 
     private static double determineMedian(int[] c) {
         return NUMBER_OF_ELEMENTS % 2 == 0 ? (double)(c[c.length - 1] + c[c.length - 2]) / 2 : c[c.length - 1];
     }
 
+    /**
+     * This is slower relative to auxillary array
+     */
     private static void findMedianInPlace() {
         int i = 0, j = 0;
+        long start = System.nanoTime();
         boolean a_flag = false;
 
         while(i + j + 2 <= MED_INDEX + 1) {
-            if (a[i] > b[j]) {
-                j++; a_flag = false;
-            } else {
-                i++; a_flag = true;
+            if (i < a.length && j < b.length) {
+                if (a[i] > b[j]) {
+                    j++;
+                    a_flag = false;
+                } else {
+                    i++;
+                    a_flag = true;
+                }
+            } else if (i >= a.length) {
+                j++;
+                a_flag = false;
+            } else if (j >= b.length) {
+                i++;
+                a_flag = true;
             }
         }
 
-//        stdout("i: " + i + " j: " + j);
-        stdout("Median is = " + determineMedian(i, j, a_flag));
-//        stdout("Median is : " + determineMedian(i, j));
+//        stdout("i: " + i + " j: " + j + " flag: " + a_flag);
+        double median = determineMedian(i, j, a_flag);
+        long time_inplace = System.nanoTime() - start;
+        stdout("Time for In Place algorithm: " + time_inplace / 1000);
+        stdout("Median is = " + median);
     }
 
     private static double determineMedian(int i, int j, boolean a_flag) {
@@ -63,10 +91,12 @@ public class MedianOfArrays {
 //        stdout("max: " + max_value);
         int min_value = 0;
         if (NUMBER_OF_ELEMENTS % 2 == 0) {
-            min_value = a_flag ? Math.max(a[i - 2], b[j - 1]) : Math.max(b[j - 2], a[i - 1]);
+            min_value = a_flag ? (i >=2 && j >= 1 ? Math.max(a[i - 2], b[j - 1]) : (j != 0 ? b[j - 1] : a[i - 2])) :
+                    (j >= 2 && i >= 1 ? Math.max(b[j - 2], a[i - 1]) : (i != 0 ? a[i - 1] : b[j - 2]));
         }
 
 //        stdout("min: " + min_value);
-        return min_value != 0 ? (double) (max_value + min_value) / 2 : max_value;
+        double median = min_value != 0 ? (double) (max_value + min_value) / 2 : max_value;
+        return median;
     }
 }
