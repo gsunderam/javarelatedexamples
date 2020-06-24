200e27b717760a4ec7809c07c668ec0b01389f4a
gsunderam <gsunder23@rediffmail.com>
2020-06-24 15:37:19 -0700

    Knapsack solved

--- src/com/sg/dp/knapsack/Knapsack.java
+++ src/com/sg/dp/knapsack/Knapsack.java
@@ -6,11 +6,20 @@ import java.util.Map;
 
 import static com.sg.dp.log.Logger.stdout;
 
+/**
+ * Knapsack solved in Java. Time Complexity = num(items N) * Capacity = NC
+ */
 public class Knapsack {
-    private static final int [] weights = {0, 3, 5, 7, 9, 11};
-    private static final int [] values = {0, 1, 4, 7, 8, 9};
-    private static final int CAPACITY = 14;
+    private static final int [] weights = {0, 2, 3, 4, 4, 5, 9}; //items
+    private static final int [] values = {0, 3, 4, 3, 5, 8, 10}; //profits for each item
+    private static final int CAPACITY = 20;
+
+    /** Populate this table so there is no need to recompute values */
     private static final int [][] knapsackTable = new int[weights.length][CAPACITY + 1];
+
+    /** Map to keep track of the items selected for the best solution. Key will be the profit value like
+     *  10. Value will be - delimited rows to keep like so "4-3-2". The last element will have the
+     *  best solution */
     private static final Map<Integer, String> map = new HashMap<>();
 
     private void printTable() {
@@ -22,20 +31,21 @@ public class Knapsack {
     }
 
     public void solve() {
-        for (int i = 1; i < weights.length; i++) {
-            for (int j = 1; j <= CAPACITY ; j++) {
-                if (weights[i] > j) {
-                    knapsackTable[i][j] = knapsackTable[i - 1][j];
+        for (int item = 1; item < weights.length; item++) {
+            for (int w = 1; w <= CAPACITY ; w++) {
+                if (weights[item] > w) { //if item wt > knapsack size, get it from prev row.
+                    knapsackTable[item][w] = knapsackTable[item - 1][w];
                 } else {
-                    int prevValue = knapsackTable[i - 1][j];
-                    int residue = knapsackTable[i - 1][j - weights[i]];
-                    int currentValue = values[i] + residue;
+                    int prevValue = knapsackTable[item - 1][w];
+                    int residue = knapsackTable[item - 1][w - weights[item]];
+                    int currentValue = values[item] + residue;
                     if (prevValue > currentValue) {
-                        knapsackTable[i][j] = prevValue;
+                        knapsackTable[item][w] = prevValue;
                     } else {
-                        knapsackTable[i][j] = currentValue;
-                        String s = map.get(residue) != null ? map.get(residue) : "";
-                        map.put(currentValue, i + "-" + s);
+                        knapsackTable[item][w] = currentValue;
+                        /** Get the prev stored rows and append it to current one using - delimiter */
+                        String prevRows = map.get(residue) != null ? map.get(residue) : "";
+                        if (!map.containsKey(currentValue)) map.put(currentValue, item + "-" + prevRows);
                     }
                 }
             }
@@ -46,6 +56,18 @@ public class Knapsack {
         Knapsack knapsack = new Knapsack();
         knapsack.solve();
         knapsack.printTable();
-        stdout(map.get(knapsackTable[weights.length - 1][CAPACITY]));
+        knapsack.solution();
+    }
+
+    /**
+     * Max profit will be in the last element of the knapsackTable. Map with this key value
+     * will have the - delimited items to select
+     **/
+    private void solution() {
+        int profit = knapsackTable[weights.length - 1][CAPACITY];
+        String [] bestRows = map.get(profit).split("-");
+        stdout("Items (index) to select are: " + Arrays.toString(bestRows));
+        Integer capacity = Arrays.stream(bestRows).map(row -> weights[Integer.valueOf(row)]).reduce(0, Integer::sum);
+        stdout(String.format("Max Profit = %d and capacity = %d ", profit,  capacity));
     }
 }
